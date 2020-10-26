24a7eb8a3ceea93d64836cb5171f80e644fc178f
bamin0422 <bamin0422@gmail.com>
2020-10-26 19:15:28 +0900

    Add homefragMaker

--- app/src/main/java/h2mud2/ganpanproject/gandan/fragment/home_fragment.kt
+++ app/src/main/java/h2mud2/ganpanproject/gandan/fragment/home_fragment.kt
@@ -6,19 +6,35 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
 import h2mud2.ganpanproject.gandan.R
+import h2mud2.ganpanproject.gandan.R.*
 
 class home_fragment: Fragment() {
+
+    var sampleImages = arrayOf(drawable.carousel1, drawable.carousel2, drawable.carousel3, drawable.carousel4)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        val view = inflater.inflate(layout.home_fragment, container, false)
+        var carouselView = view.findViewById(R.id.item_carousel) as CarouselView
+
+        carouselView.pageCount = sampleImages.size
+        carouselView.setImageListener(imageListener)
+
 
         return view
+    }
+
+    var imageListener = ImageListener(){ i: Int, imageView: ImageView ->
+        imageView.setImageResource((sampleImages[i]))
     }
 }
