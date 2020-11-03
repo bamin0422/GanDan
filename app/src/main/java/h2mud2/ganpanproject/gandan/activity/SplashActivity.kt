8931b462e98b413e1b9e088ce18390ca4a0ed3d8
hakgyun12 <gkrrbs1018@kookmin.ac.kr>
2020-11-03 10:39:28 +0900

    Add LoginActivity

--- app/src/main/java/h2mud2/ganpanproject/gandan/activity/SplashActivity.kt
+++ app/src/main/java/h2mud2/ganpanproject/gandan/activity/SplashActivity.kt
@@ -12,7 +12,7 @@ class SplashActivity: AppCompatActivity() {
         setContentView(R.layout.intro)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, 2000)
     }
--- app/src/main/java/h2mud2/ganpanproject/gandan/fragment/account_fragment.kt
+++ app/src/main/java/h2mud2/ganpanproject/gandan/fragment/account_fragment.kt
@@ -1,21 +1,25 @@
 package h2mud2.ganpanproject.gandan.fragment
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import h2mud2.ganpanproject.gandan.R
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton
 
-class account_fragment: Fragment() {
+class account_fragment: Fragment(){
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.account_fragment, container, false)
+
+        val view = inflater.inflate(R.layout.account_fragment, container, false)
+
+        return view
     }
 }
